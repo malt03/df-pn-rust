eba287bcc3471a64f8c91ec2a5ca0787c5a018bb
malt03 <k@malt03.com>
2024-05-15 00:45:47 +0900

    removedb

--- src/db.rs
+++ src/db.rs
@@ -6,6 +6,10 @@ use rocksdb::{self, BlockBasedOptions, Cache, Options};
 pub(crate) type Key = [u8; 8];
 
 pub(crate) fn open_with_cache<P: AsRef<Path>>(path: P) -> Result<DB, rocksdb::Error> {
+    if path.as_ref().exists() {
+        std::fs::remove_dir_all(&path).unwrap();
+    }
+
     let cache = Cache::new_lru_cache(1024 * 1024 * 1024 * 12); // 12GiB
     let mut table_options = BlockBasedOptions::default();
     table_options.set_block_cache(&cache);
@@ -20,28 +24,6 @@ pub(crate) trait Entity: Encode + Decode {
     fn get_key(&self) -> Key;
 }
 
-#[derive(Debug)]
-pub(crate) enum StructDBError {
-    EncodeError(bincode::error::EncodeError),
-    DecodeError(bincode::error::DecodeError),
-    DBError(rocksdb::Error),
-}
-impl From<bincode::error::EncodeError> for StructDBError {
-    fn from(error: bincode::error::EncodeError) -> Self {
-        StructDBError::EncodeError(error)
-    }
-}
-impl From<bincode::error::DecodeError> for StructDBError {
-    fn from(error: bincode::error::DecodeError) -> Self {
-        StructDBError::DecodeError(error)
-    }
-}
-impl From<rocksdb::Error> for StructDBError {
-    fn from(error: rocksdb::Error) -> Self {
-        StructDBError::DBError(error)
-    }
-}
-
 pub(crate) fn get_entity<T>(db: &DB, key: &Key) -> Option<T>
 where
     T: Entity,
@@ -52,25 +34,6 @@ where
     let (entity, _): (T, _) =
         bincode::decode_from_slice(&bytes, bincode::config::standard()).unwrap();
     Some(entity)
-}
-
-pub(crate) fn get_entities<'a, T, K, I>(db: &DB, keys: I) -> impl Iterator<Item = Option<T>> + 'a
-where
-    K: AsRef<[u8]>,
-    T: Entity,
-    I: IntoIterator<Item = K>,
-    <I as IntoIterator>::IntoIter: 'a,
-{
-    db.multi_get(keys).into_iter().map(|r| -> Option<T> {
-        match r.unwrap() {
-            Some(b) => {
-                let (entity, _) =
-                    bincode::decode_from_slice(&b, bincode::config::standard()).unwrap();
-                Some(entity)
-            }
-            None => None,
-        }
-    })
 }
 
 pub(crate) fn put_entity<T>(db: &DB, entity: &T)
